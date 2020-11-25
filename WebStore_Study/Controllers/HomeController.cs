9499cb2e04adf7b7a38fbe3768cea9eb0e8b06bc
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-11-25 16:01:07 +0200

    Первый контроллер

---
+++ WebStore_Study/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_Study.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("aslkdjas");
+            // return View();
+        }
+        public IActionResult SecondAction()
+        {
+            return Content("asdasd");
+        }
+    }
+}
