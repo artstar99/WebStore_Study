f1b10726dbb0b7bd33b857a6814ae9f29bc0c918
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-03 13:10:20 +0200

    Добавлены модель блога, модель файла изображения, внесены данные блогов в TestData

--- WebStore_Study/Data/TestData.cs
+++ WebStore_Study/Data/TestData.cs
@@ -8,6 +8,53 @@ namespace WebStore_Study.Data
 {
     public static class TestData
     {
+        public static List<Blog> Blogs { get; } = new List<Blog>()
+        {
+            new Blog
+            {
+                Id=1,
+                Heading="GIRLS PINK T SHIRT ARRIVED IN STORE",
+                Author="Mac Doe",
+                MainText=loremText,
+                CreationTime= new DateTime(2013, 12, 5, 13, 33, 0),
+                Tags=new List<string>(){"Pink", "T-Shirt", "Girls"},
+                Image=new BlogFile()
+                { Id =1, Name=$"Blogimage-1", Path="~/images/blog/blog-one.jpg"  }
+            },
+            new Blog
+            {
+                Id=2,
+                Heading="GIRLS PINK T SHIRT ARRIVED IN STORE",
+                Author="Mac Doe",
+                MainText=loremText,
+                CreationTime= new DateTime(2013, 12, 5, 13, 33, 0),
+                Tags=new List<string>(){"Pink", "T-Shirt", "Girls"},
+                Image=new BlogFile() { Id =2, Name=$"Blogimage-2",Path="~/images/blog/blog-two.jpg"}
+            },
+            new Blog
+            {
+                Id=3,
+                Heading="GIRLS PINK T SHIRT ARRIVED IN STORE",
+                Author="Mac Doe",
+                MainText=loremText,
+                CreationTime= new DateTime(2013, 12, 5, 13, 33, 0),
+                Tags=new List<string>(){"Pink", "T-Shirt", "Girls"},
+                Image=new BlogFile()
+                { Id =3, Name=$"Blogimage-3", Path="~/images/blog/blog-three.jpg"  }
+            },
+            new Blog
+            {
+                Id=4,
+                Heading="GIRLS PINK T SHIRT ARRIVED IN STORE",
+                Author="Mac Doe",
+                MainText=loremText,
+                CreationTime= new DateTime(2013, 12, 5, 13, 33, 0),
+                Tags=new List<string>(){"Pink", "T-Shirt", "Girls"},
+                Image=new BlogFile()
+                { Id =4, Name=$"Blogimage-4", Path="~/images/blog/blog-four.jpg"  }
+            }
+        };
+
         public static List<Employee> Employees { get; } = new List<Employee>()
         {
             new(){Id=1, LastName="Иванов", FirstName="Иван", Patronymic="Иванович", Age=31},
@@ -15,5 +62,26 @@ namespace WebStore_Study.Data
             new(){Id=3, LastName="Сидоров", FirstName="Сидор", Patronymic="Сидорович", Age=33},
             new(){Id=4, LastName="Константинов", FirstName="Константин", Patronymic="Константинович", Age=34}
         };
+
+        public static string loremText = @"Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+        dolore eu fugiat nulla pariatur.
+
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa
+        qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+        unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
+        beatae vitae dicta sunt explicabo.
+
+        Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+        sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+
+        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+        adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore
+        et dolore magnam aliquam quaerat voluptatem.";
+
     }
 }
---
+++ WebStore_Study/Models/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore_Study.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        /// <summary>Заголовок</summary>
+        public string Heading { get; set; }
+        /// <summary>Изображение</summary>
+        public BlogFile Image { get; set; }
+        /// <summary>Текст блога</summary>
+        public string MainText { get; set; }
+        /// <summary>Автор/summary>
+        public string Author { get; set; }
+        /// <summary>Дата создания/summary>
+        public DateTime CreationTime { get; set; }
+        /// <summary>Тэги/summary>
+        public List<string> Tags { get; set; }
+    }
+}
