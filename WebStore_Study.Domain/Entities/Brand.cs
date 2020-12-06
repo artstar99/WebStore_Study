6a64dfbdbbd1d7324cbf6cc904ad8ae388772072
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-06 18:16:01 +0200

    Сущности предметной области

---
+++ WebStore_Study.Domain/Entities/Brand.cs
@@ -0,0 +1,10 @@
+﻿using WebStore_Study.Domain.Entities.Base;
+using WebStore_Study.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_Study.Domain.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
