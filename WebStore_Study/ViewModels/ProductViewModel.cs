5867ab03b106dc3230fa5a68d7e4b7416c9a79c3
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-07 16:32:17 +0200

    Модели-представления для товара и каталога товаров

---
+++ WebStore_Study/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_Study.Domain.Entities.Base.Interfaces;
+
+namespace WebStore_Study.ViewModels
+{
+    public class ProductViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
