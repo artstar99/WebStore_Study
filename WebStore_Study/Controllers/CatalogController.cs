ef05388c6fb6c4d910d83d74e8585014454730ae
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-07 18:10:38 +0200

    Работа каталога товаров и бокового меню

---
+++ WebStore_Study/Controllers/CatalogController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_Study.Domain;
+using WebStore_Study.Infrastructure.Interfaces;
+using WebStore_Study.ViewModels;
+
+namespace WebStore_Study.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IProductData productData;
+
+        public CatalogController(IProductData productData)
+        {
+            this.productData = productData;
+        }
+        public IActionResult Shop(int? brandId, int? sectionId)
+        {
+            var filter = new ProductFilter { BrandId=brandId, SectionId=sectionId};
+            var products = productData.GetProducts(filter);
+            return View(new CatalogViewModel 
+            {
+                SectionId=sectionId,
+                BrandId=brandId,
+                Products= products
+                .OrderBy(p=>p.Order)
+                .Select(p=> new ProductViewModel 
+                {
+                    Id=p.Id,
+                    Name=p.Name,
+                    Order=p.Order,
+                    Price=p.Price,
+                    ImageUrl=p.ImageUrl,
+                })
+            } );
+        }
+    }
+}
