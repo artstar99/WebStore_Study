fbadada9aadc6deb65943a08a4a2bb933ff5c463
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-09 17:11:12 +0200

    Модель БД

--- WebStore_Study.Domain/Entities/Base/Entity.cs
+++ WebStore_Study.Domain/Entities/Base/Entity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore_Study.Domain.Entities.Base.Interfaces;
 
@@ -7,6 +9,7 @@ namespace WebStore_Study.Domain.Entities.Base
 {
     public abstract class Entity : IEntity
     {
+       [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
        public int Id { get; set; }
     }
 }
--- WebStore_Study.Domain/Entities/Base/NamedEntity.cs
+++ WebStore_Study.Domain/Entities/Base/NamedEntity.cs
@@ -1,9 +1,11 @@
-﻿using WebStore_Study.Domain.Entities.Base.Interfaces;
+﻿using System.ComponentModel.DataAnnotations;
+using WebStore_Study.Domain.Entities.Base.Interfaces;
 
 namespace WebStore_Study.Domain.Entities.Base
 {
     public abstract class NamedEntity : Entity, INamedEntity
     {
+        [Required]
         public string Name { get; set; }
     }
 }
