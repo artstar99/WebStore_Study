4b56cfee3d0e6f1b06521104ef0ea523e5ddd049
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-09 17:26:52 +0200

    Контекст БД

---
+++ WebStore_Study.DAL/Context/WebStore_StudyDb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using WebStore_Study.Domain.Entities;
+
+namespace WebStore_Study.DAL.Context
+{
+    public class WebStore_StudyDb:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Section> Sections { get; set; }
+
+        public WebStore_StudyDb(DbContextOptions<WebStore_StudyDb> options) :base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+            
+        }
+    }
+}
