0b10d0f2af09831697d6a36aa47c3d45270e89b1
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-10 15:07:31 +0200

    Сервис данных для Блогов из SQL

--- WebStore_Study/Infrastructure/UserServicesInjection.cs
+++ WebStore_Study/Infrastructure/UserServicesInjection.cs
@@ -17,7 +17,8 @@ namespace WebStore_Study.Infrastructure
         public static IServiceCollection AddUserServices(this IServiceCollection services)
         {
             services.AddTransient<IEmployeesData, InMemeoryEmplyeesData>();
-            services.AddTransient<IBlogService, InmemoryBlogService>();
+            //services.AddTransient<IBlogService, InmemoryBlogService>();
+            services.AddTransient<IBlogService, SqlBlogData>();
             //services.AddTransient<IProductData, InmemeoryProductData>();
             services.AddTransient<IProductData, SqlProductData>();
             services.AddTransient<WebStore_StudyDbInitializer>();
