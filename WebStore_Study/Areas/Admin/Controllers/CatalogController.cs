fc22b7799c0c59b5385e17b66cf1ff4f5ac78bd4
artstar99 <67787403+artstar99@users.noreply.github.com>
2020-12-23 13:30:23 +0200

    Выход/вход из области администратора

--- WebStore_Study/Areas/Admin/Controllers/CatalogController.cs
+++ WebStore_Study/Areas/Admin/Controllers/CatalogController.cs
@@ -3,12 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using WebStore_Study.Domain.Entities;
+using WebStore_Study.Infrastructure.Interfaces;
 
 namespace WebStore_Study.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin")][Authorize(Roles = Role.Administrator)]
     public class CatalogController : Controller
     {
+        private readonly IProductData productData;
+
+        public CatalogController(IProductData productData)
+        {
+            this.productData = productData;
+        }
         public IActionResult Index()
         {
             return View();
