0e0f658d7ccc8aea07bea67c9f28b59d2277c579
artstar99 <67787403+artstar99@users.noreply.github.com>
2021-01-18 20:39:22 +0200

    Клиент тестового WebApi

---
+++ Services/WebStore_Study.Clients/Base/BaseClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebStore_Study.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected HttpClient Http { get; }
+        public string Address { get; }
+
+        protected BaseClient(HttpClient client, string Address )
+        {
+            Http = client;
+            this.Address = Address;
+        }
+    }
+}
