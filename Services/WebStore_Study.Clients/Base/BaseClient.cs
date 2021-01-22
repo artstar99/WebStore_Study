9c1e20609c8c1fee86c637812b76618ab88de980
artstar99 <67787403+artstar99@users.noreply.github.com>
2021-01-22 17:45:33 +0200

    BaseClient

--- Services/WebStore_Study.Clients/Base/BaseClient.cs
+++ Services/WebStore_Study.Clients/Base/BaseClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace WebStore_Study.Clients.Base
 {
@@ -24,5 +25,47 @@ namespace WebStore_Study.Clients.Base
             };
             this.Address = Address;
         }
+
+        #region GET
+        protected T Get<T>(string url) => GetAsync<T>(url).Result;
+        protected async Task<T> GetAsync<T>(string url)
+        {
+            var response = await Http.GetAsync(url);
+            return await response.EnsureSuccessStatusCode().Content.ReadAsAsync<T>();
+        }
+        #endregion
+
+
+        #region POST
+        protected HttpResponseMessage Post<T>(string url, T item) => PostAsync(url, item).Result;
+        protected async Task<HttpResponseMessage> PostAsync<T>(string url, T item)
+        {
+            var response = await Http.PostAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+        #endregion
+
+        #region PUT
+        protected HttpResponseMessage Put<T>(string url, T item) => PutAsync(url, item).Result;
+        protected async Task<HttpResponseMessage> PutAsync<T>(string url, T item)
+        {
+            var response = await Http.PutAsJsonAsync(url, item);
+            return response.EnsureSuccessStatusCode();
+        }
+        #endregion
+
+        #region DELETE
+        protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+        protected async Task<HttpResponseMessage> DeleteAsync(string url)
+        {
+            var response = await Http.DeleteAsync(url);
+            return response;
+        } 
+        #endregion
+
+
+
+
+
     }
 }
