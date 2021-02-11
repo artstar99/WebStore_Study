abf365d1d2aa045a09ac3b760b6408131e85a43a
artstar99 <67787403+artstar99@users.noreply.github.com>
2021-02-11 20:55:28 +0200

    правим слияние

--- Services/WebStore_Study.Clients/Identity/RolesClient.cs
+++ Services/WebStore_Study.Clients/Identity/RolesClient.cs
@@ -7,6 +7,7 @@ using WebStore_Study.Clients.Base;
 using WebStore_Study.Domain.Entities;
 using WebStore_Study.Interfaces.Contracts;
 using WebStore_Study.Interfaces.Services.Identity;
+using WebStore_Study.Services.Contracts.V1;
 
 
 namespace WebStore_Study.Clients.Identity
--- Services/WebStore_Study.Interfaces/Contracts/ApiRoutes.cs
+++ Services/WebStore_Study.Interfaces/Contracts/ApiRoutes.cs
@@ -6,13 +6,22 @@ namespace WebStore_Study.Services.Contracts.V1
 {
     public static class ApiRoutes
     {
+        public static class Version1
+        {
+            private const string v1 = "v1";
 
-        public const string Values = "api/values";
+            public const string Values = "api" + v1 + "/values";
 
-        public const string Products = "api/products";
+            public const string Products = "api" + v1 + "/products";
 
-        public const string Orders = "api/orders";
+            public const string Orders = "api" + v1 + "/orders";
 
+            public static class Identity
+            {
+                public const string User = "api" + v1 + "/users";
+                public const string Role = "api" + v1 + "/roles";
+            }
+        }
 
     }
 }
