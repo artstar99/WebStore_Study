092d4ee32780fae3527efb9c1d528bbafca81411
artstar99 <67787403+artstar99@users.noreply.github.com>
2021-02-12 16:12:18 +0200

    Raw taghelper

---
+++ Ui/WebStore_Study/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore_Study.TagHelpers
+{
+    [HtmlTargetElement(Attributes = attributeName)]
+    public class ActiveRoute: TagHelper
+    {
+        private const string attributeName = "is-active-route";
+
+        [HtmlAttributeName("asp-action")]
+        public string Action { get; set; }
+
+        [HtmlAttributeName("asp-controller")]
+        public string Controller { get; set; }
+
+        [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+        public Dictionary<string, string> RoutesValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+        [ViewContext, HtmlAttributeNotBound]
+        public ViewContext VoewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll(attributeName);
+        }
+    }
+}
