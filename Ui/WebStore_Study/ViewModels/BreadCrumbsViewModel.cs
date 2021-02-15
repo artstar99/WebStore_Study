e4b30491c1c120a00df8bd955535abe4c316cf66
artstar99 <67787403+artstar99@users.noreply.github.com>
2021-02-15 19:21:39 +0200

    catalog breadcrumbs

---
+++ Ui/WebStore_Study/ViewModels/BreadCrumbsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore_Study.Domain.Entities;
+
+namespace WebStore_Study.ViewModels
+{
+    public class BreadCrumbsViewModel
+    {
+        public Section Section { get; set; }
+        public Brand Brand { get; set; }
+        public Product Product { get; set; }
+    }
+}
